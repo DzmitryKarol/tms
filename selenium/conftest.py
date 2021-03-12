afc2b319592bdcc47e507dda938224f3172c6b93
DzmitryKarol <69243036+DzmitryKarol@users.noreply.github.com>
2021-03-12 14:22:48 +0300

    Update conftest.py

--- selenium/conftest.py
+++ selenium/conftest.py
@@ -4,8 +4,12 @@ from selenium import webdriver
 
 @pytest.fixture()
 def browser():
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    browser.implicitly_wait(10)
-    yield browser
-    browser.quit()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
